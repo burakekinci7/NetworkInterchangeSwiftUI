affc8576893119dd0c1a2b30742fcecba1649ae4
burakekinci7 <ekincib015@gmail.com>
2023-12-07 23:02:43 +0300

    Initial Commit

---
+++ NetworkInterchangeSwiftUI/NetworkInterchangeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangeSwiftUIApp.swift
+//  NetworkInterchangeSwiftUI
+//
+//  Created by Ramazan Burak Ekinci on 7.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
